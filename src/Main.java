b2d3023d1a7ed9378cc3fc2b3ba71a79a74ce800
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-10-08 17:27:22 +0300

    onemoreclasscalculation

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int ticketPrice = 100000;
+        int myMiles = service.calculate(ticketPrice);
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        int ticketPrice = 287986;
-        int bonusMile = 20;
-        int bonus = ticketPrice / bonusMile;
-        int bonusSumm = bonus;
-
-
-        // Press Shift+F9 to start debugging your code. We have set one breakpoint
-        // for you, but you can always add more by pressing Ctrl+F8.
-        System.out.println(bonusSumm);
+        System.out.println(myMiles);
 
     }
 }
